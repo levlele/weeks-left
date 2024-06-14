dc387a17b81fb031c1f34a8f42f74766892f5a81
Leandro Vilanova <leandro.vilanova@univtec.com>
2024-06-14 16:23:24 -0300

    Update barrels.

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,6 +1,7 @@
 export * from "./button";
 export * from "./card";
 export * from "./dropdown-menu";
+export * from "./form";
 export * from "./input";
 export * from "./label";
 export * from "./resizable";
