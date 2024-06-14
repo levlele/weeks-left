2404d11ec432ccc30101bdd0b088c83f43631209
Leandro Vilanova <leandro.vilanova@univtec.com>
2024-06-14 16:25:19 -0300

    Remove unused imports. Update home with new components.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,28 +2,21 @@
 import { useState } from "react";
 import { calculateDaysLived, calculateSleepTime } from "@/lib/utils";
 import {
-  Input,
-  Button,
   Card,
   CardContent,
-  CardHeader,
-  CardTitle,
-  CardDescription,
   ResizableHandle,
   ResizablePanel,
   ResizablePanelGroup,
-  Label,
   Separator,
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
   Skeleton,
+  CardFooter,
 } from "@/components/ui";
-import { renderWeekBoxes } from "@/components/render-boxes";
-import { ModeToggle } from "@/components/mode-toggle";
+import {
+  ModeToggle,
+  CalculateForm,
+  DataTable,
+  renderWeekBoxes,
+} from "@/components/";
 
 export default function Home() {
   const [birthDate, setBirthDate] = useState<string>("1984-05-15");
@@ -47,9 +40,9 @@ export default function Home() {
   const [workYearsLeft, setWorkYearsLeft] = useState<number>(0);
 
   const [sleepHoursPerDay, setSleepHoursPerDay] = useState<number>(8);
-  const [sleepHoursPerWeek, setSleepHoursPerWeek] = useState<number>(0);
-  const [sleepHoursPerMonth, setSleepHoursPerMonth] = useState<number>(0);
-  const [sleepHoursPerYear, setSleepHoursPerYear] = useState<number>(0);
+  // const [sleepHoursPerWeek, setSleepHoursPerWeek] = useState<number>(0);
+  // const [sleepHoursPerMonth, setSleepHoursPerMonth] = useState<number>(0);
+  // const [sleepHoursPerYear, setSleepHoursPerYear] = useState<number>(0);
 
   const [sleepDaysLeft, setSleepDaysLeft] = useState<number>(0);
   const [sleepWeeksLeft, setSleepWeeksLeft] = useState<number>(0);
@@ -83,11 +76,7 @@ export default function Home() {
     const workWeeksLeft = Math.floor(workDaysLeft / 7);
     setWorkWeeksLeft(workWeeksLeft);
 
-    const sleepTime = calculateSleepTime(
-      sleepHoursPerDay,
-      lifeExpectancy,
-      yearsLived
-    );
+    const sleepTime = calculateSleepTime(sleepHoursPerDay, yearsLifeLeft);
     setSleepDaysLeft(Math.floor(sleepTime.days));
     setSleepWeeksLeft(Math.floor(sleepTime.weeks));
     setSleepMonthsLeft(Math.floor(sleepTime.months));
@@ -97,180 +86,55 @@ export default function Home() {
   return (
     <div className="relative min-h-screen">
       <ModeToggle className="absolute top-8 right-8" />
-      <div className="max-w-7xl mx-auto p-8 font-mono absolute inset-x-0">
+      <div className="max-w-7xl mx-auto p-8 font-mono ">
         <h1 className="text-2xl font-bold">
           The Scare Jump Procrastinator Calculator
         </h1>
-        <main className="flex gap-8 mt-8">
+        <main className="flex gap-8 mt-8 min-h-screen">
           <ResizablePanelGroup direction="horizontal">
             <ResizablePanel defaultSize={35}>
               <Card className="font-mono p-4 rounded-none">
-                <CardHeader>
-                  <CardTitle>Calculate Weeks Left</CardTitle>
-                  <CardDescription>
-                    Enter your birth date to calculate the time you have lived.
-                  </CardDescription>
-                </CardHeader>
-                <CardContent>
-                  <Label htmlFor="birthDate">Birth Date</Label>
-                  <Input
-                    id="birthDate"
-                    type="date"
-                    value={birthDate}
-                    className="mt-2 mb-4"
-                    onChange={(e) => setBirthDate(e.target.value)}
-                  />
-                  <Label htmlFor="lifeExpectancy">
-                    Life Expectancy In Your Country
-                  </Label>
-                  <Input
-                    id="lifeExpectancy"
-                    type="number"
-                    value={lifeExpectancy}
-                    className="mt-2 mb-4"
-                    onChange={(e) => setLifeExpectancy(Number(e.target.value))}
-                    placeholder="Expected Life Expectancy"
-                  />
-                  <Label htmlFor="yearsWorked">Years Worked</Label>
-                  <Input
-                    id="yearsWorked"
-                    type="number"
-                    value={yearsWorked}
-                    className="mt-2 mb-4"
-                    onChange={(e) => setYearsWorked(Number(e.target.value))}
-                    placeholder="Years Worked"
-                  />
-                  <Label htmlFor="retirementAge">Retirement Age</Label>
-                  <Input
-                    id="retirementAge"
-                    type="number"
-                    value={retirementAge}
-                    className="mt-2 mb-4"
-                    onChange={(e) => setRetirementAge(Number(e.target.value))}
-                    placeholder="Retirement Age"
-                  />
-                  <Label htmlFor="sleepHoursPerDay">Sleep Hours Per Day</Label>
-                  <Input
-                    id="sleepHoursPerDay"
-                    type="number"
-                    value={sleepHoursPerDay}
-                    className="mt-2 mb-4"
-                    onChange={(e) =>
-                      setSleepHoursPerDay(Number(e.target.value))
-                    }
-                    placeholder="Sleep Hours Per Day"
-                  />
-
-                  <Button onClick={handleCalculate} className="mt-4">
-                    Calculate
-                  </Button>
-                </CardContent>
+                <CalculateForm
+                  birthDate={birthDate}
+                  setBirthDate={setBirthDate}
+                  lifeExpectancy={lifeExpectancy}
+                  setLifeExpectancy={setLifeExpectancy}
+                  yearsWorked={yearsWorked}
+                  setYearsWorked={setYearsWorked}
+                  retirementAge={retirementAge}
+                  setRetirementAge={setRetirementAge}
+                  sleepHoursPerDay={sleepHoursPerDay}
+                  setSleepHoursPerDay={setSleepHoursPerDay}
+                  handleCalculate={handleCalculate}
+                />
                 <Separator />
-                <CardContent>
-                  <Table>
-                    <TableHeader>
-                      <TableRow>
-                        <TableHead>Time Spent</TableHead>
-                        <TableHead className="text-right">Days</TableHead>
-                        <TableHead className="text-right">Weeks</TableHead>
-                        <TableHead className="text-right">Years</TableHead>
-                      </TableRow>
-                    </TableHeader>
-                    <TableBody>
-                      <TableRow>
-                        <TableCell className="flex items-center gap-2">
-                          <span className="block size-4 bg-blue-500" />
-                          <span className="leading-none">Life Lived</span>
-                        </TableCell>
-                        <TableCell className="text-right">
-                          {daysLived ? daysLived : "0"}
-                        </TableCell>
-                        <TableCell className="text-right">
-                          {weeksLived ? weeksLived : "0"}
-                        </TableCell>
-                        <TableCell className="text-right">
-                          {yearsLived ? yearsLived : "0"}
-                        </TableCell>
-                      </TableRow>
-                      <TableRow>
-                        <TableCell className="flex items-center gap-2">
-                          <span className="block size-4 bg-blue-700" />
-                          <span className="leading-none">Life Worked</span>
-                        </TableCell>
-                        <TableCell className="text-right">
-                          {daysWorked}
-                        </TableCell>
-                        <TableCell className="text-right">
-                          {weeksWorked}
-                        </TableCell>
-                        <TableCell className="text-right">
-                          {yearsWorked}
-                        </TableCell>
-                      </TableRow>
-                      <TableRow>
-                        <TableCell className="flex items-center gap-2">
-                          <span className="block size-4 bg-red-400" />
-                          <span className="leading-none">Work Left</span>
-                        </TableCell>
-                        <TableCell className="text-right">
-                          {workDaysLeft}
-                        </TableCell>
-                        <TableCell className="text-right">
-                          {workWeeksLeft}
-                        </TableCell>
-                        <TableCell className="text-right">
-                          {workYearsLeft}
-                        </TableCell>
-                      </TableRow>
-                      <TableRow>
-                        <TableCell className="flex items-center gap-2">
-                          <span className="block size-4 bg-purple-500" />
-                          <span className="leading-none">Sleep Left</span>
-                        </TableCell>
-                        <TableCell className="text-right">
-                          {sleepDaysLeft}
-                        </TableCell>
-                        <TableCell className="text-right">
-                          {sleepWeeksLeft}
-                        </TableCell>
-                        <TableCell className="text-right">
-                          {sleepYearsLeft}
-                        </TableCell>
-                      </TableRow>
-                      <TableRow>
-                        <TableCell className="flex items-center gap-2">
-                          <span className="block size-4 bg-red-500" />
-                          <span className="leading-none">Life Left</span>
-                        </TableCell>
-                        <TableCell className="text-right">
-                          {daysLifeLeft}
-                        </TableCell>
-                        <TableCell className="text-right">
-                          {weeksLifeLeft}
-                        </TableCell>
-                        <TableCell className="text-right">
-                          {yearsLifeLeft}
-                        </TableCell>
-                      </TableRow>
-                    </TableBody>
-                  </Table>
-                </CardContent>
+                <DataTable
+                  daysLived={daysLived}
+                  weeksLived={weeksLived}
+                  yearsLived={yearsLived}
+                  daysWorked={daysWorked}
+                  weeksWorked={weeksWorked}
+                  yearsWorked={yearsWorked}
+                  workDaysLeft={workDaysLeft}
+                  workWeeksLeft={workWeeksLeft}
+                  workYearsLeft={workYearsLeft}
+                  sleepDaysLeft={sleepDaysLeft}
+                  sleepWeeksLeft={sleepWeeksLeft}
+                  sleepYearsLeft={sleepYearsLeft}
+                  daysLifeLeft={daysLifeLeft}
+                  weeksLifeLeft={weeksLifeLeft}
+                  yearsLifeLeft={yearsLifeLeft}
+                />
               </Card>
             </ResizablePanel>
             <ResizableHandle withHandle />
-
             <ResizablePanel defaultSize={65}>
-              <Card className="rounded-none p-4 h-full">
-                <CardHeader>
-                  <CardTitle>Weeks displayed as boxes</CardTitle>
-                  <CardDescription>
-                    Each line represents a year of 52 weeks.
-                    <br />
-                    The boxes represent the weeks in that year.
-                  </CardDescription>
-                </CardHeader>
+              <Card className="rounded-none py-4 px-2 h-full">
                 <CardContent>
-                  <div className="flex flex-row justify-between gap-1 pl-4">
+                  <div className="text-center pt-6 pb-1">
+                    <h3 className="text-[12px] leading-none h-4">Weeks</h3>
+                  </div>
+                  <div className="flex flex-row justify-between gap-1 pl-8">
                     {Array.from({ length: 52 }).map((_, i) => (
                       <div key={i} className="text-[7px] w-full text-right">
                         {i + 1}
@@ -278,6 +142,11 @@ export default function Home() {
                     ))}
                   </div>
                   <div className="flex gap-1 mt-1">
+                    <div className="flex items-center w-4">
+                      <h3 className="text-[12px] leading-none vertical rotate-180">
+                        Years
+                      </h3>
+                    </div>
                     <div className="flex flex-col items-end justify-between w-3">
                       {Array.from({
                         length:
@@ -306,6 +175,12 @@ export default function Home() {
                     </div>
                   </div>
                 </CardContent>
+                <CardFooter className="flex-col items-start pl-14">
+                  <p className="text-xs">
+                    * Each row represents a year of 52 weeks, each box
+                    represents a week.
+                  </p>
+                </CardFooter>
               </Card>
             </ResizablePanel>
           </ResizablePanelGroup>
