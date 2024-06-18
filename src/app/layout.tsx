b992ef3d6b634864ef34cb89b857c77a7906a2aa
Leandro Vilanova <leandro.vilanova@univtec.com>
2024-06-18 00:17:54 -0300

    Add Metadata.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,23 @@ import type { Metadata } from "next";
 import { GeistMono } from "geist/font/mono";
 
 export const metadata: Metadata = {
-  title: "The Scare Jump Procrastinator Calculator",
+  title: "Weeks Left: Countdown to Doom",
   description: "Calculate your remaining life years!",
+  authors: [{ name: "Leandro Vilanova", url: "https://leandro-vilanova.com/" }],
+  keywords: [
+    "life expectancy calculator",
+    "weeks left to live",
+    "life span calculation",
+    "life duration estimator",
+    "life expectancy tracker",
+    "years to weeks converter",
+    "healthspan estimation",
+    "longevity calculator",
+    "time remaining calculator",
+    "personal life expectancy",
+  ],
+  themeColor: "#c5ff48",
+  colorScheme: "dark light",
 };
 
 export default function RootLayout({
