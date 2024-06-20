8c6de85a842b238d48045fb1802290cbeccb4fa0
Leandro Vilanova <leandro.vilanova@univtec.com>
2024-06-20 00:04:09 -0300

    Add JSDoc.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,6 +5,12 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
+/**
+ * @description Calculate the days lived from a birth date
+ * @param {string} birthDateStr - The birth date in string format
+ * @returns {number} The days lived
+ * @example calculateDaysLived("1990-01-01") // returns 11688
+ */
 export const calculateDaysLived = (birthDateStr: string): number => {
   const birthDate = new Date(birthDateStr);
   const actualDate = new Date();
@@ -12,6 +18,14 @@ export const calculateDaysLived = (birthDateStr: string): number => {
   return Math.floor(timeDiff / (1000 * 3600 * 24));
 };
 
+/**
+ *
+ * @description Calculate the time spent sleeping
+ * @param {number} hoursSlept - The hours slept per day
+ * @param {number} yearsLifeLeft - The years left to live
+ * @returns {{ days: number; weeks: number; months: number; years: number }} The time spent sleeping
+ * @example calculateSleepTime(8, 80) // returns { days: 23360, weeks: 3340, months: 778, years: 64 }
+ */
 export const calculateSleepTime = (
   hoursSlept: number,
   yearsLifeLeft: number
@@ -24,6 +38,13 @@ export const calculateSleepTime = (
   return { days, weeks, months, years };
 };
 
+/**
+ * @description Calculate the time spent working
+ * @param {number} hoursWorked - The hours worked per day
+ * @param {number} workYearsLeft - The years left to work
+ * @returns {{ days: number; weeks: number; years: number }} The time spent working
+ * @example calculateWorkTime(8, 40) // returns { days: 11600, weeks: 1657, years: 32 }
+ */
 export const calculateWorkTime = (
   hoursWorked: number,
   workYearsLeft: number
