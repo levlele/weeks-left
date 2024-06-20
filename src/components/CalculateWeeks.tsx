ecc5c26717485ee649f4792f8ba91509f09ac06b
Leandro Vilanova <leandro.vilanova@univtec.com>
2024-06-20 01:29:50 -0300

    Update placeholder texts. Fix types.

--- src/components/CalculateWeeks.tsx
+++ src/components/CalculateWeeks.tsx
@@ -15,15 +15,15 @@ import { useLanguage } from "@/context/LanguageContext";
 interface CalculateWeeksProps {
   birthDate: string;
   setBirthDate: (value: string) => void;
-  lifeExpectancy: number;
+  lifeExpectancy: number | null;
   setLifeExpectancy: (value: number) => void;
-  yearsWorked: number;
+  yearsWorked: number | null;
   setYearsWorked: (value: number) => void;
-  workHoursPerDay: number;
+  workHoursPerDay: number | null;
   setWorkHoursPerDay: (value: number) => void;
-  retirementAge: number;
+  retirementAge: number | null;
   setRetirementAge: (value: number) => void;
-  sleepHoursPerDay: number;
+  sleepHoursPerDay: number | null;
   setSleepHoursPerDay: (value: number) => void;
   handleCalculate: () => void;
 }
@@ -51,6 +51,7 @@ export function CalculateWeeks({
       label: texts[language].life_expectancy,
       type: "number",
       value: lifeExpectancy,
+      placeholder: texts[language].life_expectancy_placeholder,
       disabled: true,
       onChange: (e: ChangeEvent<HTMLInputElement>) =>
         setLifeExpectancy(Number(e.target.value)),
@@ -60,6 +61,7 @@ export function CalculateWeeks({
       label: texts[language].birth_date,
       type: "date",
       value: birthDate,
+      placeholder: texts[language].birth_date_placeholder,
       disabled: false,
       onChange: (e: ChangeEvent<HTMLInputElement>) =>
         setBirthDate(e.target.value),
@@ -69,6 +71,7 @@ export function CalculateWeeks({
       label: texts[language].years_worked,
       type: "number",
       value: yearsWorked,
+      placeholder: texts[language].years_worked_placeholder,
       disabled: false,
       onChange: (e: ChangeEvent<HTMLInputElement>) =>
         setYearsWorked(Number(e.target.value)),
@@ -78,6 +81,7 @@ export function CalculateWeeks({
       label: texts[language].hours_worked_per_day,
       type: "number",
       value: workHoursPerDay,
+      placeholder: texts[language].hours_worked_per_day_placeholder,
       disabled: false,
       onChange: (e: ChangeEvent<HTMLInputElement>) =>
         setWorkHoursPerDay(Number(e.target.value)),
@@ -87,6 +91,7 @@ export function CalculateWeeks({
       label: texts[language].retirement_age,
       type: "number",
       value: retirementAge,
+      placeholder: texts[language].retirement_age_placeholder,
       disabled: false,
       onChange: (e: ChangeEvent<HTMLInputElement>) =>
         setRetirementAge(Number(e.target.value)),
@@ -96,6 +101,7 @@ export function CalculateWeeks({
       label: texts[language].sleep_hours_per_day,
       type: "number",
       value: sleepHoursPerDay,
+      placeholder: texts[language].sleep_hours_per_day_placeholder,
       disabled: false,
       onChange: (e: ChangeEvent<HTMLInputElement>) =>
         setSleepHoursPerDay(Number(e.target.value)),
@@ -122,10 +128,10 @@ export function CalculateWeeks({
             <Input
               id={field.id}
               type={field.type}
-              value={field.value}
+              value={field.value as string | number}
               className="mt-2 mb-4"
               onChange={field.onChange}
-              placeholder={field.label}
+              placeholder={field.placeholder}
               disabled={field.disabled}
             />
           </div>
